1c270106052c6172cd4c5cd4b65216fa1cfac220
Natalie Holton <natalie.k.holton@gmail.com>
2015-12-17 17:53:26 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Quiztwo::Application.config.secret_key_base = 'a5708d53336d249478f959994d719108b6754b637f853f117ca47702cdb4087684700ee1e1d211a816085cc26c9afa4b059178119ccb8554947ff47e10d0ab2e'
